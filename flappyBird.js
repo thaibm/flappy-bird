db369c8fffef1b0afb2eca2a5f6c3e9f5888b53c
Thaibm <thaibm.uet@gmail.com>
2020-08-26 22:08:39 +0700

    Add simple index.html

---
+++ flappyBird.js
@@ -0,0 +1,14 @@
+var cvs = document.getElementById('canvas');
+var ctx = cvs.getContext('2d');
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
+
+bird.src = 'images/bird.png';
+bg.src = 'images/background.png';
+fg.src = 'images/fg.png';
+pipeNorth.src = 'images/pipe-green.png';
+pipeSouth.src = 'images/pipe-green.png';
